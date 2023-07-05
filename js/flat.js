2ac16d884b9ef1940caabf3e00d94321b85c76ee
jueun3521 <jueun1004juju@gamil.com>
2023-07-05 16:00:10 +0900

    갤러리 모바일, 태블릿 화면 완성

--- js/flat.js
+++ js/flat.js
@@ -1,13 +1,18 @@
-window.onload = function () {
-    var menuToggleBtn = document.querySelector(".menu_toggle_btn");
-    var gnb = document.querySelector(".gnb");
+// window.onload = function () {
+//     var menuToggleBtn = document.querySelector(".menu_toggle_btn");
+//     var gnb = document.querySelector(".gnb");
 
-    menuToggleBtn.addEventListener("click", function () {
-        // if (gnb.style.display === "none") {
-        //     gnb.style.display = "block";
-        // } else{
-        //     gnb.style.display = "none";
-        // }
-        gnb.style.display = (gnb.style.display === "none") ? "block" : "none";
+//     menuToggleBtn.addEventListener("click", function () {
+//         // if (gnb.style.display === "none") {
+//         //     gnb.style.display = "block";
+//         // } else{
+//         //     gnb.style.display = "none";
+//         // }
+//         gnb.style.display = (gnb.style.display === "none") ? "block" : "none";
+//     });
+// };
+jQuery(function ($) {
+    $(".menu_toggle_btn").click(function(){
+        $(".gnb").stop().slideToggle("fast");
     });
-};
+});
